3177d1d909a346377fd0b2b60ece770156a6fcff
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-08 22:15:54 +0000

    automatically backed up by learn

---
+++ instance_methods.rb
@@ -0,0 +1,29 @@
+class Dog 
+  def bark=(dog_bark)
+  @this_dog_bark=(dog_bark)
+  end
+
+  def bark
+  @this_dog_bark
+  end 
+end
+
+dog = Dog.new
+dog.bark = "woof!"
+
+puts dog.bark
+
+class sit 
+  def sit=(dog_sit)
+  @this_dog_bark=
+  end
+
+  def bark
+  @this_dog_bark
+  end 
+end
+
+dog = Dog.new
+dog.bark = "dog barks"
+
+puts dog.bark
